84da7df654932b9059587e0b6dc3f0342cdf0665
AAhlbrandt <alec@ahlbrandt.net>
2019-03-28 15:15:16 -0500

    Add translate

--- GLToolbox/GLToolbox.cpp
+++ GLToolbox/GLToolbox.cpp
@@ -1,6 +1,11 @@
+#include <array>
+
 class GLToolbox {
 public:
 	GLToolbox() {
 
+	}
+	static std::array<float, 3> translate(std::array<float, 3> coordinates, std::array<float, 3> dCoordinates) {
+		return { coordinates[0] + dCoordinates[0], coordinates[1] + dCoordinates[1], coordinates[2] + dCoordinates[2] };
 	}
 };
