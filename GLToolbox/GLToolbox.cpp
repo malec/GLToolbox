ad43a03d6652ecd00cf8f06ed16c4837cfb37ff5
AAhlbrandt <alec@ahlbrandt.net>
2019-03-28 15:34:57 -0500

    add rotations

--- GLToolbox/GLToolbox.cpp
+++ GLToolbox/GLToolbox.cpp
@@ -1,11 +1,26 @@
 #include <array>
 
-class GLToolbox {
+static class GLToolbox {
 public:
-	GLToolbox() {
-
-	}
 	static std::array<float, 3> translate(std::array<float, 3> coordinates, std::array<float, 3> dCoordinates) {
 		return { coordinates[0] + dCoordinates[0], coordinates[1] + dCoordinates[1], coordinates[2] + dCoordinates[2] };
+	}
+	static std::array<float, 3> rotateX(std::array<float, 3> coordinates, float theta) {
+		float x = coordinates[0];
+		float y = coordinates[1];
+		float z = coordinates[2];
+		return { x, y*cos(theta) - z * sin(theta), y*sin(theta) + z * cos(theta) };
+	}
+	static std::array<float, 3> rotateY(std::array<float, 3> coordinates, float theta) {
+		float x = coordinates[0];
+		float y = coordinates[1];
+		float z = coordinates[2];
+		return { x*cos(theta) + z * sin(theta), y, -x * sin(theta) + z * cos(theta) };
+	}
+	static std::array<float, 3> rotateZ(std::array<float, 3> coordinates, float theta) {
+		float x = coordinates[0];
+		float y = coordinates[1];
+		float z = coordinates[2];
+		return { x*cos(theta) - y * sin(theta), x*sin(theta) + y * cos(theta), z };
 	}
 };
